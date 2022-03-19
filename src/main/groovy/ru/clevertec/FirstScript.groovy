230f900112224694cbaf7e701509dcbf0fb368ad
Sergey Korolenko <pavilion1900@gmail.com>
2022-03-20 00:35:23 +0300

    initial commit

---
+++ src/main/groovy/ru/clevertec/FirstScript.groovy
@@ -0,0 +1,4 @@
+package ru.clevertec
+
+println "Hello my world"
+
---
+++ src/main/groovy/ru/clevertec/HelloWorld.groovy
@@ -0,0 +1,9 @@
+package ru.clevertec
+
+class HelloWorld {
+    int value
+
+    static void main(String[] args) {
+        println "Hello world"
+    }
+}
