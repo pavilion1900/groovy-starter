f2902f76d501188f8b0c94cedc907554c41c09e5
Sergey Korolenko <pavilion1900@gmail.com>
2022-03-20 13:55:59 +0300

    6. Операции с переменными

---
+++ src/main/groovy/ru/clevertec/lesson6/OperatorsRunner.groovy
@@ -0,0 +1,11 @@
+package ru.clevertec.lesson6
+
+assert 1 + 2 == 3
+assert 4 - 3 == 1
+assert 3 * 5 == 15
+assert 3 / 2 == 1.5
+assert 3.intdiv(2) == 1
+assert (int) (3 / 2) == 1
+assert (3 / 2) as Integer == 1
+assert 10 % 3 == 1
+assert 2**3 == 8
